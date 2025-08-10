d3cbf956d53e7abac0d0cc5583bc287fe2c63b00
Salman Nazeer <salmannazeer477@gmail.com>
2025-08-10 20:17:03 +0530

    changed the array name from stack to infix in preparation for implementing postfix conversion

--- evaluator.c
+++ evaluator.c
@@ -2,34 +2,38 @@
 #include<string.h>
 #include<ctype.h>
 const char* expression = "(20 + 40)*2";
-char stack[50][50];
+char infix[50][50];
 int main(){
+    // Tokenize expression
     int i = 0, j = 0;
     int len = strlen(expression);
     for(int k = 0; k < len; k++){
         if(expression[k] == ' ') continue;
         if(!isdigit(expression[k])){
-            stack[i][j] = expression[k];
-            stack[i][j + 1] = '\0';
+            infix[i][j] = expression[k];
+            infix[i][j + 1] = '\0';
             i++; j = 0;
         }
         else{
             if(isdigit(expression[k - 1])){
                 i--; j++;
-                stack[i][j] = expression[k];
-                stack[i][j + 1] = '\0';
+                infix[i][j] = expression[k];
+                infix[i][j + 1] = '\0';
                 i++; j = 0;
             }
             else{
-                stack[i][j] = expression[k];
-                stack[i][j + 1] = '\0';
+                infix[i][j] = expression[k];
+                infix[i][j + 1] = '\0';
                 i++; j = 0;
             }
         }
     }
 
     for(int k = 0; k < i; k++){
-        puts(stack[k]);
+        puts(infix[k]);
     }
+    /////////////////////////////////
+
+    
 
 }
