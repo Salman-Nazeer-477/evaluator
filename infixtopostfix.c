f4de7bd77d4fb31e28492f79f8b16884e99e3d79
Salman <salmannazeer477@gmail.com>
2026-02-13 12:46:39 +0530

    Fixed infixtopostfix converter

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -3,8 +3,8 @@
 #include <ctype.h>
 #include <math.h>
 
-char infix[50][50] = {"1", "+", "2"};
-int l_infix = 3;
+char infix[50][50] = {"(", "1", "+", "2", ")", "*", "5"};
+int l_infix = 7;
 int isoperator(char c);
 int isnumber(char c);
 int isprecedencelower(char a, char b);
@@ -14,49 +14,64 @@ int main()
 {
     char postfix[50][50];
     char stack[50][50];
-    int i_postfix = -1;
-    int i_stack = -1;
+    int l_postfix = -1;
+    int l_stack = -1;
     int i_infix;
-    for(i_infix = 0; i_infix < l_infix; i_infix++){
-        if(isnumber(*infix[i_infix])){
-            i_postfix++;
-            strcpy(postfix[i_postfix], infix[i_infix]);
-        } else if(isparenthesis(*infix[i_infix])){
-            printf("is a parenthesis\n");
-            if(*infix[i_infix] == '('){
-                i_postfix++;
-                strcpy(postfix[i_postfix], infix[i_infix]);
-            } else {
-                while(*stack[i_stack] != '('){
-                    i_postfix++;
-                    strcpy(postfix[i_postfix], stack[i_stack]);
-                    i_stack--;
+    for (i_infix = 0; i_infix < l_infix; i_infix++)
+    {
+        if (isnumber(*infix[i_infix]))
+        {
+            l_postfix++;
+            strcpy(postfix[l_postfix], infix[i_infix]);
+        }
+        else if (isparenthesis(*infix[i_infix]))
+        {
+            if (*infix[i_infix] == '(')
+            {
+                l_stack++;
+                strcpy(stack[l_stack], "(");
+            }
+            else
+            {
+                while (l_stack > -1 && *stack[l_stack] != '(')
+                {
+                    l_postfix++;
+                    strcpy(postfix[l_postfix], stack[l_stack]);
+                    l_stack--;
                 }
-                i_stack--;
+                l_stack--;
             }
-        } else if(isoperator(*infix[i_infix])){
-            printf("is an operator\n");
-            if(isparenthesis(*stack[i_stack])){
-                i_stack++;
-                strcpy(stack[i_stack], infix[i_infix]);
+        }
+        else if (isoperator(*infix[i_infix]))
+        {
+            if (isparenthesis(*stack[l_stack]))
+            {
+                l_stack++;
+                strcpy(stack[l_stack], infix[i_infix]);
             }
-            else if(isprecedencelower(*infix[i_infix], *stack[i_stack])){
-                i_postfix++;
-                strcpy(postfix[i_postfix], stack[i_stack]);
-                i_stack--;
+            else if (l_stack > -1 && isprecedencelower(*infix[i_infix], *stack[l_stack]))
+            {
+                l_postfix++;
+                strcpy(postfix[l_postfix], stack[l_stack]);
+                l_stack--;
             }
-            else {
-                i_stack++;
-                strcpy(stack[i_stack], infix[i_infix]);
+            else
+            {
+                l_stack++;
+                strcpy(stack[l_stack], infix[i_infix]);
             }
         }
     }
-    while(i_stack > -1){
-        i_postfix++;
-        strcpy(postfix[i_postfix], stack[i_stack]);
-        i_stack--;
+    while (l_stack > -1)
+    {
+        l_postfix++;
+        strcpy(postfix[l_postfix], stack[l_stack]);
+        l_stack--;
     }
-    for(int k = 0; k <= i_postfix; k++) {printf("%s\n", postfix[k]);}
+    for (int k = 0; k <= l_postfix; k++)
+    {
+        printf("%s\n", postfix[k]);
+    }
 }
 
 int isoperator(char c)
@@ -64,28 +79,36 @@ int isoperator(char c)
     return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
 }
 
-int isnumber(char c){
+int isnumber(char c)
+{
     return c >= '0' && c <= '9';
 }
 
-int isprecedencelower(char a, char b){
-    switch(a){
-        case '^':
+int isprecedencelower(char a, char b)
+{
+    switch (a)
+    {
+    case '^':
+        return 0;
+        break;
+    case '*':
+    case '/':
+        if (b == '^')
+            return 1;
+        else
             return 0;
-            break;
-        case '*':
-        case '/':
-            if(b == '^') return 1;
-            else return 0;
-            break;
-        case '+':
-        case '-':
-            if(b == '+' || b == '-') return 0;
-            else return 1;
-            break;
+        break;
+    case '+':
+    case '-':
+        if (b == '+' || b == '-')
+            return 0;
+        else
+            return 1;
+        break;
     }
 }
 
-int isparenthesis(char c){
+int isparenthesis(char c)
+{
     return c == '(' || c == ')';
 }
